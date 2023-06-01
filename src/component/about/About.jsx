22d94c385a816e2a98a17d4c17d763d09799f85d
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2023-06-01 13:31:00 -0400

    about

---
+++ src/component/about/About.jsx
@@ -0,0 +1,192 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Header from "../Home/Header";
+import Loading from "../../more/Loader";
+import MetaData from "../../more/MetaData";
+import "./About.css";
+import Footer from "../../more/Footer";
+// import BottomTab from "../../more/BottomTab";
+
+const About = () => {
+  const { loading } = useSelector((state) => state.profile);
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <MetaData title="About" />
+          <div>
+            <Header />
+            <div
+              style={{
+                width: "90%",
+                margin: "0px auto",
+              }}
+            >
+              <div className="about__page">
+                {/* 1st verse */}
+                <div className="row flex">
+                  <div className="col__2">
+                    <img src="https://res.cloudinary.com/kingsly/image/upload/v1677429600/bbb_ym5jqg.jpg" />
+                  </div>
+                  <div className="col__2">
+                    <div className="meta">
+                      <span
+                        style={{
+                          fontSize: "40px",
+                          fontWeight: "700",
+                          lineHeight: "1.2",
+                        }}
+                      >
+                        Welcome to Kapada online store
+                      </span>
+                      <p>
+                        "Kapada" is a name that comes from the Nepali language, which is the official language of Nepal. In Nepali, "khel" means sport or game, and "kud" means play or participation. Together, "Kapada" can be translated to mean "sports play" or "participation in sports".
+
+                        A sports website named "Kapada" could be intended to serve the Nepalese community or those interested in Nepalese sports. The name itself is a nod to Nepal's rich sporting culture, which includes popular sports like football (soccer), cricket, volleyball, and martial arts. In recent years, Nepal has also made strides in sports like mountaineering and trekking, with Nepalese athletes achieving world-renowned success in these areas.
+                      </p>
+                      <p>
+                        Overall, a website named "Kapada" could aim to promote and celebrate Nepalese sports and sporting achievements, as well as provide a platform for sports fans to engage with one another and access relevant news, information, and products related to their favorite sports.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                {/* 2nd verse
+                <div className="second">
+                  <div className="heading">
+                    <h2>What We Provide?</h2>
+                  </div>
+                  <div className="row flex">
+                    <div className="col__3">
+                      <div
+                        style={{
+                          padding: "10px",
+                          border: "1px solid rgb(0 0 0 / 14%)",
+                          minHeight: "230px",
+                        }}
+                      >
+                        <div className="flex align__items__center justify__content__center image">
+                          <img src="http://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-1.svg" />
+                        </div>
+                        <span>Best Prices & Offers</span>
+                        <p>
+                          There are many variations of passages of sport wares
+                          available, but the majority have suffered alteration
+                          in some form
+                        </p>
+                      </div>
+                    </div>
+                    <div className="col__3">
+                      <div
+                        style={{
+                          padding: "10px",
+                          border: "1px solid rgb(0 0 0 / 14%)",
+                          minHeight: "230px",
+                        }}
+                      >
+                        <div className="flex align__items__center justify__content__center image">
+                          <img src="http://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-2.svg" />
+                        </div>
+                        <span>Best For Trust & Quality</span>
+                        <p>
+                          There are many variations of passages of Lorem Ipsum
+                          available, but the majority have suffered alteration
+                          in some form
+                        </p>
+                      </div>
+                    </div>
+                    <div className="col__3">
+                      <div
+                        style={{
+                          padding: "15px",
+                          border: "1px solid rgb(0 0 0 / 14%)",
+                          minHeight: "230px",
+                        }}
+                      >
+                        <div className="flex align__items__center justify__content__center image">
+                          <img src="http://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-3.svg" />
+                        </div>
+                        <span>Fast Delivery System</span>
+                        <p>
+                          There are many variations of passages of Lorem Ipsum
+                          available, but the majority have suffered alteration
+                          in some form
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="col__3">
+                      <div
+                        style={{
+                          padding: "15px",
+                          border: "1px solid rgb(0 0 0 / 14%)",
+                          minHeight: "230px",
+                        }}
+                      >
+                        <div className="flex align__items__center justify__content__center image">
+                          <img src="http://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-4.svg" />
+                        </div>
+                        <span>Easy Returns Service</span>
+                        <p>
+                          There are many variations of passages of Lorem Ipsum
+                          available, but the majority have suffered alteration
+                          in some form
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="col__3">
+                      <div
+                        style={{
+                          padding: "15px",
+                          border: "1px solid rgb(0 0 0 / 14%)",
+                          minHeight: "230px",
+                        }}
+                      >
+                        <div className="flex align__items__center justify__content__center image">
+                          <img src="http://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-5.svg" />
+                        </div>
+                        <span>100% satisfication</span>
+                        <p>
+                          There are many variations of passages of Lorem Ipsum
+                          available, but the majority have suffered alteration
+                          in some form
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="col__3">
+                      <div
+                        style={{
+                          padding: "15px",
+                          border: "1px solid rgb(0 0 0 / 14%)",
+                          minHeight: "230px",
+                        }}
+                      >
+                        <div className="flex align__items__center justify__content__center image">
+                          <img src="http://wp.alithemes.com/html/nest/demo/assets/imgs/theme/icons/icon-6.svg" />
+                        </div>
+                        <span>Great Daily Deal</span>
+                        <p>
+                          There are many variations of passages of Lorem Ipsum
+                          available, but the majority have suffered alteration
+                          in some form
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div> */}
+              </div>
+            </div>
+            <Footer />
+          </div>
+          {/* <BottomTab /> */}
+        </>
+      )}
+    </>
+  );
+};
+
+export default About;
